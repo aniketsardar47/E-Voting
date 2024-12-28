bd9b8573553be5d4ba81e5dcff6e99beaaffe942
aniketsardar47 <aniketsardar89@gmail.com>
2024-12-28 22:24:28 +0530

    Adding blockchain codes

---
+++ Source Code/Blockchain/VARIABLES.h
@@ -0,0 +1,18 @@
+#ifdef VARIABLES_H
+#define VARIABLES_H
+
+#include<cstring>
+#include<iostream>
+
+const uint32_t blockid;
+char* voter_id;
+uint64_t nonce;
+time_t timestamp;
+uint32_t choice;
+const char* prev_hash;
+char* hash;
+
+
+
+
+#endif
---
+++ Source Code/Blockchain/blockchain.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include "SHA256.h"
+#include "VARIABLES.h"
+using namespace std;
+
+
